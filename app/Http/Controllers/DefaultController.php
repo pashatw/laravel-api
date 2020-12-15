605c47319a5a2e05ea5573814f5c51e7e41fd167
Pasha Wiraharja <pashatw.dev@gmail.com>
2020-12-15 13:13:26 +0700

    add theme

---
+++ app/Http/Controllers/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DefaultController extends Controller
+{
+    public function index(Request $request)
+    {
+    	return view('pages.dashboard');
+    }
+}
